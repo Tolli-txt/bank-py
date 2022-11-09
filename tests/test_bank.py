460efff9af8d5bf48f67c21b31ce94e4c7b8fc58
Tolli-txt <70858693+Tolli-txt@users.noreply.github.com>
2022-11-09 19:00:04 +0100

    Removed comments

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -38,39 +38,3 @@ def test_understanding_client_customer_ptp():
 @patch('builtins.input', side_effect=[1])
 def test_view_specific_account(input):
     assert client1.view_specific_account() == 1
-
-
-# def test_pack_the_payload_customer_2():
-#     test_dict = {
-#         "name": "pyTest",
-#         "balance": 2000
-#     }
-#     assert customer1.ptp({"action": 1, "data": test_dict}) == b'{"action": 1, "data": test_dict}'
-
-
-#
-# @patch('client.get_input', return_value="Antanas")
-# @patch('client.get_input', return_value=1337)
-# def test_new_account_dump(name, balance):
-#     print(name)
-#     print(balance)
-
-#assert client1.new_account_dump(lambda x: 1) == (1, 1)
-
-# def test_new_account_dump(input, money):
-#     print(input)
-#     print(money)
-
-# def test_new_account_dump(monkeypatch):
-#     inputs = iter(['Testguy', 1337])
-#     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-#     result = client1.new_account_dump()
-#     assert result == "New account created"
-
-
-# def test_new_account_dump():
-#     client1
-#     set_keyboard_input(["venvGuy", "1337"])
-#     client1.new_account_dump()
-#     output = get_display_output()
-#     assert output == ["Enter customers name: ", "Enter account balance: "]
