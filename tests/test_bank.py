cd56a6af6e587398f9b73838eeef41c5457b0f42
Tolli-txt <70858693+Tolli-txt@users.noreply.github.com>
2022-11-09 22:41:18 +0100

    Update test_bank.py

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -31,10 +31,6 @@ def test_pack_the_payload_server():
     assert server1.ptp({"action": 1}) == b'{"action": 1}'
 
 
-def test_understanding_client_customer_ptp():
-    assert server1.ptp({"test": 1234}) == client1.ptp({"test": 1234}) == b'{"test": 1234}'
-
-
 @patch('builtins.input', side_effect=[1])
 def test_view_specific_account(input):
     assert client1.view_specific_account() == 1
